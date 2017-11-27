ae4b8b207f7734b5f3c8c28b18597e06d87fd4ae
notzoom <33988923+notzoom@users.noreply.github.com>
2017-11-27 19:42:54 +0200

    Update squirt.js

--- squirt.js
+++ squirt.js
@@ -90,7 +90,7 @@ sq.progressBarLocation = sq.progressBarLocation || 'bottom';
       function readabilityReady(){
         handler && document.removeEventListener('readility.ready', handler);
 		let article = readability.grabArticle();
-	        let article = article.innerHTML;
+	        article = article.innerHTML;
 	        article = article.replace(/\>\s+\</g,'');
 	        article.innerHTML = article;
 		// Read text
