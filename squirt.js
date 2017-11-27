ba6f3d30a863a4199ee7272f5dc5e6960f683ee4
notzoom <33988923+notzoom@users.noreply.github.com>
2017-11-27 02:05:37 +0200

    Update squirt.js

--- squirt.js
+++ squirt.js
@@ -329,15 +329,6 @@ sq.progressBarLocation = sq.progressBarLocation || 'bottom';
     return function textToNodes(text) {
       text = text.trim('\n').replace(/\s+\n/g,'\n');
       return text
-			 .replace(/ \./g, '.')
-			 .replace(/(\.[\s])(?=\”)/g, '.')
-			 .replace(/([\s](\-|\—|\–)[\s])/g, ' ')
-			 .replace(/(\(\s)(?=.)/g, '(')
-			 .replace(/(?=.)(\s\))/g, ')')
-			 .replace(/\s(?=[\;\!\,])/g, '')
-			 .replace(/[,](?![\"\”\'])/g, '$& ')
-			 .replace(/(?!^)(?=.)[^\s](?=[\"\”\'])[^\s]/g, '$& ')
-			 .replace(/([\$\£\#])(\s{1,})/g, '$1')
 	     .replace(/['][\s][s][\s]/g,  "'s ")
 	     .replace(/['][\s][t][\s]/g,  "'t ")
 	     .replace(/["][\s][.][\s]/g,  '". ')
