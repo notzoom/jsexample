d6272d119252f71e8a498c06468b4b9aed559d21
notzoom <33988923+notzoom@users.noreply.github.com>
2017-11-28 06:35:27 +0200

    Update squirt.js

--- squirt.js
+++ squirt.js
@@ -102,9 +102,6 @@ sq.progressBarLocation = sq.progressBarLocation || 'bottom';
       function readabilityReady(){
         handler && document.removeEventListener('readility.ready', handler);
 		let article = readability.grabArticle();
-	      	if (window.location.hostname.indexOf('theguardian.com') != -1){
-	            let article = document.getElementsByClassName("content__main").cloneNode(true);
-                }
 		article = removeUnwantedElements(article)
 		read(readability.grabArticleText(article));
       };
