9c95e7a7a5f1e5f58b25c215d0525c120b6c7fa1
notzoom <33988923+notzoom@users.noreply.github.com>
2017-11-29 09:07:53 +0200

    Update readability.js

--- readability.js
+++ readability.js
@@ -460,7 +460,7 @@ var readability = {
      * -right- content.
      **/
     let length = articleContent.textContent.length    
-    if ((length < 250) || (length > 250000)) {
+    if (length < 50) {
       page.innerHTML = pageCacheHtml;
 
       if (readability.flagIsActive(readability.FLAG_STRIP_UNLIKELYS)) {
