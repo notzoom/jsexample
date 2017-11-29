30aabbf32ae915953ac8cbe0faac1a91d80c09b0
notzoom <33988923+notzoom@users.noreply.github.com>
2017-11-30 00:08:59 +0200

    Update readability.js

--- readability.js
+++ readability.js
@@ -459,9 +459,10 @@ var readability = {
      * the sieve approach gives us a higher likelihood of finding the
      * -right- content.
      **/
-    
+    let article = articleContent.textContent;
     let length = articleContent.textContent.length;
-    if (length < 250) {
+    let medium = (article.match(/medium.com/g) || []).length;
+    if (length < 250 || medium > 20 ) {
       page.innerHTML = pageCacheHtml;
 
       if (readability.flagIsActive(readability.FLAG_STRIP_UNLIKELYS)) {
