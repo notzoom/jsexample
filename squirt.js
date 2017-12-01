0550d7d0bb7607bd0e801b5063c066848b36cd8d
notzoom <33988923+notzoom@users.noreply.github.com>
2017-12-01 02:53:52 +0200

    Update squirt.js

--- squirt.js
+++ squirt.js
@@ -359,6 +359,7 @@ sq.progressBarLocation = sq.progressBarLocation || 'bottom';
 	     .replace(/ \…/g,  '…')
 	     .replace(/[\s]+[)]/g,  ')')
 	     .replace(/[\s]+[;]/g,  ';')
+	     .replace(/["][.]["]/g,  '". "')
 	     .replace(/([\s](\-|\—|\–)[\s])/g, ' ')
 	     .replace(/([\0-9])([\,])(\s{1,})([\0-9])/g, '$1$2$4')
   	     .replace(/([\0-9])([\.])(\s{1,})([\0-9])/g, '$1$2$4')
